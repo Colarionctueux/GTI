6f826e7fda92b5832cc0908dbd1ecba25f51a4e8
Nicolas <nicolas.eckman.etu@univ-lille.fr>
2023-11-06 23:31:04 +0100

    starting code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,53 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+    <img src="/top-bg.svg" alt="top background">
+    <nav>
+        <img src="/Logo.svg" alt="logo GTI">
+        <div>
+            <p>Compétences</p>
+            <p>Fournisseurs</p>
+            <p>Clients</p>
+        </div>
+    </nav>
+</main>
+
+<style>
+    main>img{
+        margin: 0;
+        padding: 0;
+        z-index: -1;
+        position: absolute;
+        width: 100%;
+
+        top:0px;
+        left:0px;
+    }
+
+    nav{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        margin: 50px;
+        margin-top: 20px;
+    }
+
+    nav img{
+        width: 171;
+        height: 112;
+    }
+
+    nav p{
+        color: #002B3D;
+        margin-bottom: 0;
+        background: linear-gradient(to bottom, #ffffff00 95%, #002B3D 95%);
+        margin-left: 25px;
+        margin-right: 25px;
+        font-size: 1.6em;
+        font-family: "Prompt", sans-serif;
+        font-weight: 200;
+    }
+    
+    nav div{
+        display: flex;
+        height: 60px;
+    }
+</style>
